995e4b6d5c9d36a273a4f31f4cfd2759cc65d0d8
WillNess210 <WillNess210@gmail.com>
2019-11-21 10:42:12 -0700

    cleared old project

--- src/app/Constants.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class Constants{
-    public static final int MS_PER_TURN = 500;
-    public static final int BUTTON_WIDTH_PER_CHAR = 12;
-    public static final int MIN_BUTTON_WIDTH = 50;
-    public static final int PX_BTWN_BUTTONS = 5;
-}
